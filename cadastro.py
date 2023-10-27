182c6594fa65672529a77c3a1146622b1eee8361
Jeffim777 <jeffersonjeffim777@gmail.com>
2023-10-26 22:14:50 -0300

    Adicionando cadastro e informação .

---
+++ cadastro.py
@@ -0,0 +1,6 @@
+rua = input('Digite sua rua: ')
+numero = input('Digite o número: ')
+complemento = input('Digite o complemento: ')
+bairro = input('Digite o bairro: ')
+cep = input('Digite o CEP: ')
+print('Seu endereço é:\nRua: ', rua, '\nNº: ', numero, '\nComplemento: ', complemento, '\nBairro:' , bairro , '\nCEP:' , cep)#\n é para pular uma linha 
---
+++ informacao.py
@@ -0,0 +1,6 @@
+nome = input('Digite seu nome: ')
+sobrenome = input('Digite seu sobrenome: ')
+cidade = input('Digite sua cidade: ') # o input sozinho é usado para literal (string), ou seja, texto
+idade = int(input('Digite sua idade: ')) # int é usado para números inteiros. Exemplo: 3 -6
+peso = float(input('Digite seu peso: ')) # float é usado para números com casas decimais. Exemplo: 3.5 -2.567
+print('Seu nome completo é:', nome, sobrenome, 'da cidade de', cidade, 'que tem', idade, 'anos e pesa', peso, 'KGs')
